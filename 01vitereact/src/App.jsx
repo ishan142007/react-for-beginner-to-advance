85c5e982b7807d4003a1c098d4d7bc1cee521096
ishan <ishanpatel9785@gmail.com>
2025-08-27 12:44:36 +0530

    added notes

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   
 
   return (
-    <>
     //this fragment tag in react it is actually a div tag because in react we cant return more than one tag so we have to warp all the tags in a single tag so for this we use fragment tag.
+    <>
 
 
      <h1>chai aur react | ishan patel |to we learned about the set up of react and how to clean or project to work efficently.we have to remove all the files which are not in our use or we dont know a hang of them and with the they are not playing a vital role in the functioning of the code.</h1>
